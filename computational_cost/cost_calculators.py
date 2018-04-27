5ceb293cd1d687234f6aed415301333bf656f239
Daichi SUZUO <belltailjp@gmail.com>
2018-04-27 14:40:34 +0900

    Fixed Conv and Deconv

--- computational_cost/cost_calculators.py
+++ computational_cost/cost_calculators.py
@@ -1,3 +1,5 @@
+import math
+
 import chainer
 from chainer.utils.conv import get_conv_outsize, get_deconv_outsize
 
@@ -27,24 +29,54 @@ def calc_linear(function, in_data, **kwargs):
     return (ops, mread, mwrite)
 
 def calc_conv2d(function, in_data, **kwargs):
-    #TODO: support dilated conv
     x, W = in_data[:2]
     b = in_data[2] if len(in_data) == 3 else None
 
     batch_size, in_c, in_h, in_w = x.shape
     out_c, _, kh, kw = W.shape
+    g = function.groups
 
     out_h = get_conv_outsize(in_h, kh, function.sy, function.ph,
                              cover_all=function.cover_all, d=function.dy)
     out_w = get_conv_outsize(in_w, kw, function.sx, function.pw,
                              cover_all=function.cover_all, d=function.dx)
-    g = function.groups
-    # twice because of fused multiply-and-add
-    ops = 2 * batch_size * in_c * out_c // (g * kw * kh * out_w * out_h)
-    if b is not None:
-        ops += batch_size * out_c * out_w * out_h  # bias
 
-    return (ops, 0, 0)
+    ops = in_c * int(math.ceil(out_c / g)) * kw * kh * out_w * out_h
+    if not kwargs['unify_fma']:
+        ops *= 2
+
+    mread = x.size + W.size
+    mwrite = batch_size * out_c * out_h * out_w
+    if b is not None:
+        ops += batch_size * out_c * out_w * out_h
+        mread += b.size
+
+    return (ops * batch_size, mread, mwrite)
+
+def calc_deconv2d(function, in_data, **kwargs):
+    x, W = in_data[:2]
+    b = in_data[2] if len(in_data) == 3 else None
+
+    batch_size, in_c, in_h, in_w = x.shape
+    _, out_c, kh, kw = W.shape
+    g = function.groups
+
+    out_h = get_deconv_outsize(in_h, kh, function.sy,
+                               function.ph, d=function.dy)
+    out_w = get_deconv_outsize(in_w, kw, function.sx,
+                               function.pw, d=function.dx)
+
+    ops = in_c * int(math.ceil(out_c / g)) * kw * kh * in_w * in_h
+    if not kwargs['unify_fma']:
+        ops *= 2
+
+    mread = x.size + W.size
+    mwrite = batch_size * out_c * out_h * out_w
+    if b is not None:
+        ops += batch_size * out_c * out_w * out_h
+        mread += b.size
+
+    return (ops * batch_size, mread, mwrite)
 
 def calc_fixed_bn(function, in_data, **kwargs):
     x, _, _, mean, var = in_data
--- tests/test_cost_calculators.py
+++ tests/test_cost_calculators.py
@@ -57,6 +57,83 @@ def test_average_pooling():
     assert mwrite == (3 * 50 * 50)
 
 
+def test_conv2d_with_bias_unifyfma():
+    x = np.random.randn(1, 3, 10, 10).astype(np.float32)
+    W = np.random.randn(12, 3, 3, 3).astype(np.float32)
+    b = np.random.randn(12).astype(np.float32)
+    f = F.connection.convolution_2d.Convolution2DFunction(pad=1)
+    ops, mread, mwrite = calc_conv2d(f, [x, W, b], unify_fma=True)
+    assert ops == (12 * 10 * 10) * (3 * 3 * 3) + (12 * 10 * 10)
+    assert mread == 3 * 10 * 10 + 12 * 3 * 3 * 3 + 12
+    assert mwrite == 12 * 10 * 10
+
+def test_conv2d_nobias_unifyfma():
+    x = np.random.randn(1, 3, 10, 10).astype(np.float32)
+    W = np.random.randn(12, 3, 3, 3).astype(np.float32)
+    f = F.connection.convolution_2d.Convolution2DFunction(pad=1)
+    ops, mread, mwrite = calc_conv2d(f, [x, W], unify_fma=True)
+    assert ops == (12 * 10 * 10) * (3 * 3 * 3)
+    assert mread == 3 * 10 * 10 + 12 * 3 * 3 * 3
+    assert mwrite == 12 * 10 * 10
+
+def test_conv2d_with_bias_no_unifyfma():
+    x = np.random.randn(1, 3, 10, 10).astype(np.float32)
+    W = np.random.randn(12, 3, 3, 3).astype(np.float32)
+    b = np.random.randn(12).astype(np.float32)
+    f = F.connection.convolution_2d.Convolution2DFunction(pad=1)
+    ops, mread, mwrite = calc_conv2d(f, [x, W, b], unify_fma=False)
+    assert ops == 2 * (12 * 10 * 10) * (3 * 3 * 3) + (12 * 10 * 10)
+    assert mread == 3 * 10 * 10 + 12 * 3 * 3 * 3 + 12
+    assert mwrite == 12 * 10 * 10
+
+def test_conv2d_nobias_no_unifyfma():
+    x = np.random.randn(1, 3, 10, 10).astype(np.float32)
+    W = np.random.randn(12, 3, 3, 3).astype(np.float32)
+    f = F.connection.convolution_2d.Convolution2DFunction(pad=1)
+    ops, mread, mwrite = calc_conv2d(f, [x, W], unify_fma=False)
+    assert ops == 2 * (12 * 10 * 10) * (3 * 3 * 3)
+    assert mread == 3 * 10 * 10 + 12 * 3 * 3 * 3
+    assert mwrite == 12 * 10 * 10
+
+def test_deconv2d_with_bias_unifyfma():
+    x = np.random.randn(1, 3, 10, 10).astype(np.float32)
+    W = np.random.randn(3, 12, 3, 3).astype(np.float32)
+    b = np.random.randn(12).astype(np.float32)
+    f = F.connection.deconvolution_2d.Deconvolution2DFunction(stride=2, pad=0)
+    ops, mread, mwrite = calc_deconv2d(f, [x, W, b], unify_fma=True)
+    assert ops == (3 * 10 * 10) * (12 * 3 * 3) + (12 * 21 * 21)
+    assert mread == 3 * 10 * 10 + 3 * 12 * 3 * 3 + 12
+    assert mwrite == 12 * 21 * 21
+
+def test_deconv2d_nobias_unifyfma():
+    x = np.random.randn(1, 3, 10, 10).astype(np.float32)
+    W = np.random.randn(3, 12, 3, 3).astype(np.float32)
+    f = F.connection.deconvolution_2d.Deconvolution2DFunction(stride=2, pad=0)
+    ops, mread, mwrite = calc_deconv2d(f, [x, W], unify_fma=True)
+    assert ops == (3 * 10 * 10) * (12 * 3 * 3)
+    assert mread == 3 * 10 * 10 + 3 * 12 * 3 * 3
+    assert mwrite == 12 * 21 * 21
+
+def test_deconv2d_with_bias_no_unifyfma():
+    x = np.random.randn(1, 3, 10, 10).astype(np.float32)
+    W = np.random.randn(3, 12, 3, 3).astype(np.float32)
+    b = np.random.randn(12).astype(np.float32)
+    f = F.connection.deconvolution_2d.Deconvolution2DFunction(stride=2, pad=0)
+    ops, mread, mwrite = calc_deconv2d(f, [x, W, b], unify_fma=False)
+    assert ops == 2 * (3 * 10 * 10) * (12 * 3 * 3) + (12 * 21 * 21)
+    assert mread == 3 * 10 * 10 + 3 * 12 * 3 * 3 + 12
+    assert mwrite == 12 * 21 * 21
+
+def test_deconv2d_nobias_no_unifyfma():
+    x = np.random.randn(1, 3, 10, 10).astype(np.float32)
+    W = np.random.randn(3, 12, 3, 3).astype(np.float32)
+    f = F.connection.deconvolution_2d.Deconvolution2DFunction(stride=2, pad=0)
+    ops, mread, mwrite = calc_deconv2d(f, [x, W], unify_fma=False)
+    assert ops == 2 * (3 * 10 * 10) * (12 * 3 * 3)
+    assert mread == 3 * 10 * 10 + 3 * 12 * 3 * 3
+    assert mwrite == 12 * 21 * 21
+
+
 def test_linear_nobias_unifyfma():
     x = np.random.randn(1, 10).astype(np.float32)
     w = np.random.randn(20, 10).astype(np.float32)
