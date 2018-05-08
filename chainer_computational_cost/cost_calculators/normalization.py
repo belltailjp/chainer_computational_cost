d7bfdc8ea61559200e0f92dccefe6ac7ed67ef17
Daichi SUZUO <belltailjp@gmail.com>
2018-05-08 14:16:58 +0900

    LRN

--- chainer_computational_cost/cost_calculators/normalization.py
+++ chainer_computational_cost/cost_calculators/normalization.py
@@ -1,5 +1,7 @@
 from chainer.functions.normalization.batch_normalization \
         import FixedBatchNormalization
+from chainer.functions.normalization.local_response_normalization \
+        import LocalResponseNormalization
 
 
 def calc_fixed_bn(func: FixedBatchNormalization, in_data, **kwargs):
@@ -9,4 +11,18 @@ def calc_fixed_bn(func: FixedBatchNormalization, in_data, **kwargs):
     ops = n_elements * 2    # *2 <- scale and shift
     mread = n_elements + len(mean) + len(var)
     mwrite = n_elements
+    return (ops, mread, mwrite)
+
+
+def calc_lrn(func: LocalResponseNormalization, in_data, **kwargs):
+    x, = in_data
+
+    c = x.shape[1]
+    s = c * func.k - (func.k // 2) * 2
+    ops_square = x.size
+    ops_neighbor_sum = x.size * s + x.size * 3
+    ops_total = ops_square + ops_neighbor_sum + x.size
+    ops = ops_total
+    mread = x.size + x.shape[1] * x.shape[2] * s
+    mwrite = x.size
     return (ops, mread, mwrite)
--- tests/test_cost_calculators/test_normalization.py
+++ tests/test_cost_calculators/test_normalization.py
@@ -1,4 +1,4 @@
-import chainer.functions as F
+import chainer.functions.normalization as N
 import numpy as np
 
 import pytest
@@ -12,7 +12,7 @@ def test_fixed_bn():
     beta = np.random.randn(3).astype(np.float32)
     mean = np.random.randn(3).astype(np.float32)
     var = np.random.exponential(size=(3,)).astype(np.float32)
-    f = F.normalization.batch_normalization.FixedBatchNormalization()
+    f = N.batch_normalization.FixedBatchNormalization()
     ops, mread, mwrite = calculators[type(f)](f, [x, gamma, beta, mean, var])
 
     # in test mode BN, gamma, beta, mean and var will eventually become
@@ -20,3 +20,19 @@ def test_fixed_bn():
     assert ops == 3 * 10 * 10 * 2
     assert mread == 3 * 10 * 10 + (3 + 3)   # input data, scale and shift param
     assert mwrite == 3 * 10 * 10
+
+
+def test_lrn():     # TODO: verify formula
+    x = np.random.randn(1, 8, 10, 10).astype(np.float32)
+    f = N.local_response_normalization.LocalResponseNormalization()
+    ops, mread, mwrite = calculators[type(f)](f, [x])
+
+    # square x, neighboring sum
+    c = x.shape[1]
+    s = c * f.k - (f.k // 2) * 2    # sum of k-neighbor channels (no pad)
+    ops_square = x.size
+    ops_neighbor_sum = x.size * s + x.size * 3  # including *alpha, +k, **beta
+    ops_total = ops_square + ops_neighbor_sum + x.size
+    assert ops == ops_total
+    assert mread == x.size + x.shape[1] * x.shape[2] * s
+    assert mwrite == x.size
